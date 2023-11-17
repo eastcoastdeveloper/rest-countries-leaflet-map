da2af331d05f04f7f83998edd8b38d28b8718b4a
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2023-11-17 11:35:29 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
-import { LeafletModule } from "@asymmetrik/ngx-leaflet";
+import { HttpClientModule } from '@angular/common/http';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { FilterlistPipe } from './filter.pipe';
 
 @NgModule({
-  imports:      [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    LeafletModule
-    ],
-  declarations: [
-    AppComponent,
-    FilterlistPipe
-  ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule, HttpClientModule, FormsModule, LeafletModule],
+  declarations: [AppComponent, FilterlistPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,15 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterlist'
+  name: 'filterlist',
 })
 export class FilterlistPipe implements PipeTransform {
-
   transform(value: any, args?: any): any {
-    if(!args)
-     return value;
+    if (!args) return value;
     return value.filter(
-      item => item.name.toLowerCase().indexOf(args.toLowerCase()) > -1
-   );
+      (item) => item.name.toLowerCase().indexOf(args.toLowerCase()) > -1
+    );
   }
 }
